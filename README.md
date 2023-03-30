14a63a0a3d02446b7512075136e3eadf5527c43d
Fabio Kleis <fabiohkrc@gmail.com>
2023-03-29 23:31:51 -0300

    agora o toscop roda em multithread com mutex e a thread de refresh roda a cada 5secs, falta aplicar um trim no parse das tasks

--- README.md
+++ README.md
@@ -3,17 +3,21 @@ uma versão tosca do comando top
 trabalho para disciplina de sistemas operacionais
 
 # refs
-- https://man7.org/linux/man-pages/man2/syscalls.2.html
-- https://man7.org/linux/man-pages/man2/sysinfo.2.html
-- https://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html
-- https://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html
-- https://man7.org/linux/man-pages/man0/sys_stat.h.0p.html
-- https://linux.die.net/man/2/fstat
-- https://linux.die.net/man/3/snprintf
-- https://linux.die.net/man/3/getpwuid
-- https://man7.org/linux/man-pages/man2/nice.2.html
-- https://linux.die.net/man/3/ctime
-- https://pubs.opengroup.org/onlinepubs/7908799/xcurses/curses.h.html
-- https://man7.org/linux/man-pages/man5/proc.5.html
-- https://man7.org/linux/man-pages/man3/fscanf.3p.html
-- https://man7.org/linux/man-pages/man3/pthread_create.3.html
+- https://man7.org/linux/man-pages/man2/syscalls.2.html                # lista com todas as syscalls
+- https://man7.org/linux/man-pages/man2/sysinfo.2.html                 # struct para pegar informacoes gerais do sistema (memoria, swap...)
+- https://pubs.opengroup.org/onlinepubs/7908799/xsh/dirent.h.html      # tipo do diretorio, utilizado para ler o /proc/[pid] 
+- https://pubs.opengroup.org/onlinepubs/7908799/xsh/readdir.html       # funcao que retorna um struct dirent
+- https://man7.org/linux/man-pages/man0/sys_stat.h.0p.html             # para entender como pegar informacoes de arquivo com a funcao stat 
+- https://linux.die.net/man/3/snprintf                                 # para concatenar strings com formatacao
+- https://linux.die.net/man/3/getpwuid                                 # para obter informacoes de usuario com base no uid
+- https://man7.org/linux/man-pages/man2/nice.2.html                    # para enteder oque é nice
+- https://linux.die.net/man/3/ctime                                    # para obter o tempo do sistema com o struct tm
+- https://pubs.opengroup.org/onlinepubs/7908799/xcurses/curses.h.html  # ncurses para criar a interface do programa toscop
+- https://man7.org/linux/man-pages/man5/proc.5.html                    # man do proc para ver quais arquivos preciso ler
+- https://man7.org/linux/man-pages/man3/pthread_create.3.html          # para criar threads
+- https://man7.org/linux/man-pages/man3/pthread_attr_init.3.html       # para criar os atributo para dar join
+- https://linux.die.net/man/3/pthread_mutex_init                       # para entender as maneiras de inicializar mutex
+- https://linux.die.net/man/3/pthread_mutex_lock                       # funcao para dar lock em um mutex
+- https://linux.die.net/man/3/pthread_mutex_destroy                    # funcao para desalocar um mutex 
+- https://pubs.opengroup.org/onlinepubs/7908799/xsh/time.h.html        # para entender os variados tipos de tempo e as maneiras de mensura-lo 
+-  https://man7.org/linux/man-pages/man3/clock_gettime.3.html          # funcao para obter o tempo a partir de um tipo de CLOCK 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <bits/time.h>
 #include <curses.h>
 #include <stdio.h>
 #include <ncurses.h>
@@ -8,32 +9,53 @@
 #include <unistd.h>
 #include "term_header.h"
 
-#define MAX_TIME 5
 
-static clock_t g_start_t;
+#define MAX_TIME 5.0  // refresh a cada 5 secs
+
+
+// variaveis globais para gerenciar threads
+static pthread_mutex_t toscop_mutex = PTHREAD_MUTEX_INITIALIZER;
 static int starts_at = 0;
 static int k_p = 0;
 
+// thread para imprimir todos os processos
 typedef struct term_print_t {
     pthread_t thread_id;
     term_header* th;
-    double refresh_t;
 } term_print_t;
 
+// thread para atualizar a lista de processos
 typedef struct term_refresh_t {
     pthread_t thread_id;
     term_header* th;
+    double refresh_t;
 } term_refresh_t;
 
 
 // thread para atualizar a lista de processos
 void* refresh_th(void* arg) {
+    
     term_refresh_t* trt = arg;
+    struct timespec st, ct = {0, 0};
 
-    tl_free(trt->th); // limpa lista de processos anterior
-    trt->th = create_term_header(); // cria nova lista
-    g_start_t = clock(); // reseta o clock
+    // apenas deixa dar join se for sair do programa
+    while (k_p != 'q') {
+        clock_gettime(CLOCK_MONOTONIC, &ct); // atualiza a cada iteracao o tempo atual com tepo MONOTÔNICO
+        // calcula o tempo atual - tempo do começo
+        trt->refresh_t = (double)(ct.tv_sec - st.tv_sec) +
+            (double)(ct.tv_nsec - st.tv_nsec) / 1000000000; // nanosec por sec
 
+        // caso tenha passado 5s da refresh 
+        if (trt->refresh_t >= MAX_TIME) {
+            pthread_mutex_lock(&toscop_mutex);
+
+            tl_free(trt->th); // limpa lista de processos anterior
+            trt->th = create_term_header(); // cria nova lista
+            st = ct; // reseta o clock
+            
+            pthread_mutex_unlock(&toscop_mutex);
+        }
+    } 
     pthread_exit(NULL);
 }
 
@@ -44,24 +66,7 @@ void* print_th(void* arg) {
 
     term_print_t* tpt = arg;
 
-    // cria estrutura basica para atualizar os processos
-    // thread para atualizar o th
-    term_refresh_t refresh_thread;
-    // inicializa a thread que faz o refresh da lista de processos
-    refresh_thread.thread_id = 1;
-    refresh_thread.th = tpt->th; // aponta para o mesmo th
-
-    // atributo para dar join
-    pthread_attr_t attr;
-    int s = pthread_attr_init(&attr);
-
-    if (s != 0) {
-        fprintf(stderr, "ERROR: could not create attr with pthread_att_init: %s\n", strerror(errno));
-        pthread_exit((void *) -1);
-    }
-
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+  
     clock_t pr_t = clock();
     double p_t = 0;
 
@@ -73,56 +78,51 @@ void* print_th(void* arg) {
         // para poder navegar entre os processos
         switch (k_p) {
             case KEY_DOWN:
-                starts_at = starts_at + 1 == tpt->th->t_procs ? 0 : starts_at + 1;
-                break;
+                {
+                    pthread_mutex_lock(&toscop_mutex);
+                    starts_at = starts_at + 1 == tpt->th->t_procs ? 0 : starts_at + 1;
+                    pthread_mutex_unlock(&toscop_mutex);
+                    break;
+                }
             case KEY_UP:
-                starts_at = starts_at - 1 < 0 ? tpt->th->t_procs - 1 : starts_at - 1;
-                break;
+                {
+                    pthread_mutex_lock(&toscop_mutex);
+                    starts_at = starts_at - 1 < 0 ? tpt->th->t_procs - 1 : starts_at - 1;
+                    pthread_mutex_unlock(&toscop_mutex);
+                    break;
+                }
 
             default:
                 break;
         }
 
         if (p_t >= 0.1) { // a cada 0.1 segundos é atualizado o print
-            clear();
+            pthread_mutex_lock(&toscop_mutex);
 
+            clear(); // limpa tela 
             tl_print(tpt->th, starts_at);
             printw("\nt_procs: %d\nstarts_at: %d\n\n", tpt->th->t_procs, starts_at);
             pr_t = clock();
-
             refresh(); // escrever de fato na tela
 
-        }
+            pthread_mutex_unlock(&toscop_mutex);
 
-        tpt->refresh_t = (double)(clock() - g_start_t) / CLOCKS_PER_SEC;
-        // caso tenha passado 5s cria uma thread para dar refresh 
-        if (tpt->refresh_t >= MAX_TIME) {
-            int st = pthread_create(&refresh_thread.thread_id, &attr, refresh_th, &refresh_thread);
-            if (st) {
-                fprintf(stderr, "ERROR: could not create refresh thread with pthread_create: %s\n", strerror(errno));
-                pthread_exit((void *) -1); // para no join da main sabermos se deu erro
-            }
-            int sj = pthread_join(refresh_thread.thread_id, NULL);
-            if (sj < 0) {
-                fprintf(stderr, "ERROR: could not join refresh_thread: %s\n", strerror(errno));
-                pthread_exit((void *) -1); // para no join da main sabermos se deu erro
-            } 
-        }
-
+        } 
     }
-
 
     pthread_exit(NULL);
 }
 
 int main() {
 
+    // inicializa a tela do ncurses
     initscr();
-    timeout(0);
+    timeout(0); // para nao ficar blocando
     raw();
-    keypad(stdscr, TRUE);
-    curs_set(FALSE);
+    keypad(stdscr, TRUE); // para capturar as setinhas
+    curs_set(FALSE); 
     noecho();
+
 
     // atributo para dar join
     pthread_attr_t attr;
@@ -135,14 +135,13 @@ int main() {
 
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+    // th tem a lista de processos
     term_header* th = create_term_header();
 
     // thread para printar os processos
     term_print_t print_thread;
-
     print_thread.thread_id = 0;
     print_thread.th = th;
-    print_thread.refresh_t = 0;
 
     int st = pthread_create(&print_thread.thread_id, &attr, print_th, &print_thread); // cria thread com loop principal
 
@@ -150,6 +149,21 @@ int main() {
         fprintf(stderr, "ERROR: could not create print thread with pthread_create: %s\n", strerror(errno));
         exit(1);
     }
+    // thread para atualizar o th
+    term_refresh_t refresh_thread;
+    refresh_thread.thread_id = 1;
+    refresh_thread.th = th; // aponta para o mesmo th
+    refresh_thread.refresh_t = 0;
+
+    // inicializa a thread que faz o refresh da lista de processos
+    st = pthread_create(&refresh_thread.thread_id, &attr, refresh_th, &refresh_thread);
+    if (st) {
+        fprintf(stderr, "ERROR: could not create refresh thread with pthread_create: %s\n", strerror(errno));
+        exit(1);
+    }
+
+
+    // join das threads
 
     int sj = pthread_join(print_thread.thread_id, NULL);
     if (sj < 0) {
@@ -161,8 +175,18 @@ int main() {
         usleep(1000000);
     }
 
+    sj = pthread_join(refresh_thread.thread_id, NULL);
+    if (sj < 0) {
+        fprintf(stderr, "ERROR: could not join refresh_thread: %s\n", strerror(errno));
+        exit(1);
+    } else {
+        printw("deu join na thread de refresh %c\n", k_p);
+        refresh();
+        usleep(1000000);
+    }
 
     // free em tudo que foi usado
+    pthread_mutex_destroy(&toscop_mutex);
     pthread_attr_destroy(&attr);
     tl_free(th);
     endwin();
